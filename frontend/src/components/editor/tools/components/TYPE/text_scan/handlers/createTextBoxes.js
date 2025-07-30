017946b0e8e264b702c7aade984538b0295c7b82
Eiby777 <abisaymedinarosario@gmail.com>
2025-07-30 17:30:20 -0400

    Fix missing canvas re-render after adding text scan boxes

--- frontend/src/components/editor/tools/components/TYPE/text_scan/handlers/createTextBoxes.js
+++ frontend/src/components/editor/tools/components/TYPE/text_scan/handlers/createTextBoxes.js
@@ -64,6 +64,7 @@ const createTextBoxes = (result, canvasInstances, colorToTypeTextMap, saveState)
       canvas.discardActiveObject();
       canvas.requestRenderAll();
       saveState(textObject, ObjectStatus.ADD);
+      canvas.renderAll();
     });
 
     idArrays[canvasIndex] = canvasIds;
